e4d47cb78697917e364f33d63cddd90d2493b7d9
renato <renato.athaydes@curity.io>
2019-05-27 21:50:06 +0200

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_web/material.dart';
+import 'package:gh_review/views/pull_requests.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PR Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Arial',
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Arial'),
+        ),
+      ),
+      home: PullRequests(title: 'PR Manager'),
+    );
+  }
+}
